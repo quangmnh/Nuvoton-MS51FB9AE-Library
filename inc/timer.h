8c204fc16bde9e6ae353d3e852d71753840985e8
Quang Minh <minh.nguyenfunie1010@hcmut.edu.vn>
2021-02-11 23:56:31 +0700

    Last commit maybe

--- inc/timer.h
+++ inc/timer.h
@@ -1,6 +1,11 @@
 #ifndef TIMER_H_
 #define TIMER_H_
 
+void Timer0_Init(void)
+void Timer1_Init(void)
+void Timer0_Delay1ms(void)
+void Timer1_Delay1ms(void)
 
+extern unsigned char flag_50ms;
 
 #endif
